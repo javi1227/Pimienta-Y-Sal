0c49d41a58fe90a320b656caf74a811f979f2370
Javier Garcia <javii_93_27@hotmail.com>
2022-08-15 14:56:56 -0300

    se sube ejercicio

---
+++ src/controller/productController.js
@@ -0,0 +1,17 @@
+const{getProducts} = require("../data/products")
+
+module.exports = {
+    productDetail: (req,res) => {
+
+        let productoId = +req.params.id;
+
+        let productoSolicitado = getProducts.find((producto)=>producto.id===productoId)
+
+        
+
+        res.render("detalleMenu",{
+            
+            productoSolicitado
+        })
+    }
+}
